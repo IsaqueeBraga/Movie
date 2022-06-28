949277c53b99cb451eb79154ed4b530044138a00
Isaquee Braga <flaisaque@hotmail.com>
2022-06-28 13:16:35 -0300

    trabalho de ingles

---
+++ java.js
@@ -0,0 +1,32 @@
+let Comedy = {
+    Title: "Space Jam",
+    Director: " Joe Pytka",
+    Year: "1996",
+
+};
+
+let Action = {
+    Title:"Avengers Endgame",
+    Director: " Anthony Russo" ,
+    Year: "2019",
+};
+
+
+let Fantasy ={
+    Title:"Fantastic Beasts: Dumbledore's Secrets",
+    Director:" David Yates",
+    Year:"2022",
+};
+
+
+let movie = {Comedy, Action, Fantasy};
+
+console. log(movie)
+
+
+let favorit = "Hustle"
+if(favorit == "Hustle"){
+    console. log ("My favorit movie is");
+}else{
+    console. log ("Not even 5 stars");
+}
